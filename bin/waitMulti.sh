3d08bf0d52f3ce9a9d38e10858668a2a57ebc840
Hailin Su <cbkmephisto@gmail.com>
2017-05-02 15:16:03 -0500

    Create waitMulti.sh

---
+++ bin/waitMulti.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[ ! $# -eq 2 ] && echo usage: $0 proc_name nthreads && echo examp: $0 hsrm.coder 6 && exit
+
+
+        while [ 1 ]
+        do
+#		echo ---------------------
+#		ps -A | grep $1
+                nthreads=`ps -A | grep $1 | grep -v grep | grep -v waitMulti.sh| wc -l`
+                if [ $nthreads -lt $2 ]
+                then
+                        break
+                fi
+#		echo waiting at $nthreads: $2 inputed
+                sleep 1
+        done
